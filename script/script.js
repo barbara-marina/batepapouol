ac041bc59bd0bc2ae5fe3e729f3ef2a585f87546
barbara-marina <barbaramarinaufsm@gmail.com>
2022-02-13 03:41:26 -0300

    requisitos concluidos

--- script/script.js
+++ script/script.js
@@ -63,6 +63,7 @@ function chatScreen() {
             <footer>
                 <input id="userMessage" type="text" placeholder="Escreva aqui...">
                 <button type="button" onclick="sendMessage()" data-identifier="send-message"><ion-icon name="paper-plane-outline"></ion-icon></button>
+                <div class="private"></div>
             </footer>
         </main>
 
@@ -71,7 +72,7 @@ function chatScreen() {
             <h1>Escolha um contato para enviar mensagem:</h1>
             <ul class="participants"></ul>
             <h1>Escolha a visibilidade:</h1>
-            <ul>
+            <ul class="visibilities">
                 <li class="visibility" data-identifier="visibility" onclick="selectContact(this)">
                     <div>
                         <ion-icon name="lock-open"></ion-icon>
@@ -140,10 +141,18 @@ function filterMessages(response) {
         }
     }
     messages.childNodes[messages.childNodes.length - 2].scrollIntoView();
+
 }
 
 function sendMessage() {    
-    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', { from: userName, to: "Todos", text: document.getElementById("userMessage").value, type: "message"});
+    let typeMessage = "";
+    if(userPrivate !== "Todos") {
+        typeMessage = "private_message";
+    } else {
+        typeMessage = "message;"
+    }
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', { from: userName, to: userPrivate, text: document.getElementById("userMessage").value, type: typeMessage});
 
     document.getElementById("userMessage").value = "";
 
@@ -178,7 +187,7 @@ function filterContact(response) {
         <li class="participant" data-identifier="participant" onclick="selectContact(this)">
             <div>
                 <ion-icon name="people-sharp"></ion-icon>
-                Todos
+                <p>Todos</p>
             </div>
             <ion-icon name="checkmark-sharp" class="hidden check"></ion-icon>
         </li>
@@ -188,37 +197,48 @@ function filterContact(response) {
         participant.innerHTML += `
             <li class="participant" data-identifier="participant" onclick="selectContact(this)">
                 <div>
-                    <ion-icon name="person-circle-sharp"></ion-icon>${response.data[i].name}
+                    <ion-icon name="person-circle-sharp"></ion-icon>
+                    <p>${response.data[i].name}</p>
                 </div>
                 <ion-icon name="checkmark-sharp" class="hidden"></ion-icon>
             </li>
         `
     }
-
+    desativarClickEmMim(response);
     checkUserPrivateActivity(response);
 
 }
 
 function selectContact(selected) {
-    fetchContact();
     const participantIsSelected = document.querySelector(".participant .check");
     const visibilityIsSelected = document.querySelector(".visibility .check");
     const check =  selected.childNodes[3];
-    if ((participantIsSelected !== null) && (selected.classList.contains("participant"))) {
+    const textoDentro = selected.childNodes[1].childNodes[3].innerText;
+    const iconTodos = document.querySelector(".visibilities").childNodes[1].childNodes[3];
+    const iconQualquer = document.querySelector(".visibilities").childNodes[3].childNodes[3];
+
+    if ((participantIsSelected !== null)) {
         participantIsSelected.classList.remove("check");
         check.classList.add("check");
-        userPrivate = selected.innerText;
-    } else if ((visibilityIsSelected !== null) && (selected.classList.contains("visibility"))) {        
-        visibilityIsSelected.classList.remove("check");
-        check.classList.add("check");
+        userPrivate = selected.childNodes[1].childNodes[3].innerText;
     }
 
+    if ((textoDentro !== "Todos") && (iconTodos !== null)){
+        iconTodos.classList.remove("check");
+        iconQualquer.classList.add("check");
+        alterarHTMLPrivada();
+    } else if ((textoDentro === "Todos") && (iconQualquer !== null)) {
+        iconQualquer.classList.remove("check");
+        iconTodos.classList.add("check");
+        limparHTML();
+    }
 }
 
 function checkUserPrivateActivity(response) {
     let index = response.data.findIndex(filterUserPrivate);
     const selected = document.querySelector(".participants").childNodes[(index*2)+3];
     const selectEveryone =  document.querySelector(".participants").childNodes[1];
+
 
     if (index !== -1) {
         selectContact(selected);
@@ -230,4 +250,27 @@ function checkUserPrivateActivity(response) {
 
 function filterUserPrivate(participant) {
     return participant.name === userPrivate;
+}
+
+function filterUserName(user) {
+    return user.name === userName;
+}
+
+function desativarClickEmMim(response) {
+    let index = response.data.findIndex(filterUserName);
+    const selectedUser = document.querySelector(".participants").childNodes[(index*2)+3];
+
+    if (index !== -1) {
+        selectedUser.classList.add("desativa-click");
+    }
+}
+
+function alterarHTMLPrivada() {
+    document.querySelector(".private").innerHTML = `
+        Enviando para ${userPrivate} (reservadamente)    
+    `;
+}
+
+function limparHTML() {
+    document.querySelector(".private").innerHTML = "";
 }
